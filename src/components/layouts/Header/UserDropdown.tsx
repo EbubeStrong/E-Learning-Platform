b775fbf16ab9f5e9ba58d5be842e1c74c0141ae9
Abraham Samuel <ebubesammy567@gmail.com>
2026-09-05 15:32:27 +0100

    style: fix styling

--- src/components/layouts/Header/UserDropdown.tsx
+++ src/components/layouts/Header/UserDropdown.tsx
@@ -61,13 +61,13 @@ export default function UserDropdown() {
 
       <DropdownMenuContent
         align="start"
-        className="mt-1 w-[260px] p-1.5 z-[9999] bg-mocha-100 text-calacatta-800 border-calacatta-300 dark:bg-nero-marquina-500 dark:text-nero-marquina-100 dark:border-nero-marquina-500"
+        className="mt-1 w-[260px] p-1.5 z-[9999] bg-mocha-100 text-calacatta-800 border-calacatta-300 dark:bg-mocha-400/70 dark:text-nero-marquina-100 dark:border-nero-marquina-500"
       >
         <DropdownMenuLabel className="font-normal">
           <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
             {displayName}
           </span>
-          <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
+          <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-white">
             {email}
           </span>
         </DropdownMenuLabel>
